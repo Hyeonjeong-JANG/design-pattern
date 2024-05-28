1de3347dd0ff309c46f2c1ccbb91aa8098ea54f9
Hyeonjeong-JANG <139729358+Hyeonjeong-JANG@users.noreply.github.com>
2024-05-28 21:37:00 +0900

    Update PythonTeacher.java

--- src/ex05/teacher/PythonTeacher.java
+++ src/ex05/teacher/PythonTeacher.java
@@ -1,9 +1,5 @@
 package ex05.teacher;
 
-/**
- * 이렇게 하면 OCP는 지켰지만 DIP를 지키지 못한 것이다. 추상화 시키지 않았어.
- * 그럼 어떻게 하면 되냐? 공통된 메서드들은 공통으로 쓰고 파이썬강의하기만 추상클래스를 만들어서 따로 만들어서 쓴다.
- */
 public class PythonTeacher extends Teacher {
     @Override
     public void 강의하기() {
